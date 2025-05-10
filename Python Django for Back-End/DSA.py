14f4f068e119188ce887690f97f74c6f3c07aa4b
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-05-10 14:56:04 +0600

    Learning

---
+++ Python Django for Back-End/DSA.py
@@ -0,0 +1,30 @@
+
+# | DSA 
+# ! List
+
+emptyList = []
+print(type(emptyList))
+
+emptyTuple = ()
+print(type(emptyTuple))
+
+x = (5)
+print(type(x)) #?this is not tuple for tuple we need to use comma
+
+y = (5,)
+print(type(y))
+
+z = {}
+print(type(z)) #?this is dict not set!!!
+
+
+
+#! s = {,} this is not valid like tuple
+s=set({}) 
+print("Type: ",type(s))
+
+# ? now it is set we have to use set()
+
+
+# | Method of list
+ 
