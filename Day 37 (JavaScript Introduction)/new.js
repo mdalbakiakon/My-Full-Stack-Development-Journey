a765ca1548f749be37d44234f78ac1b4dcf8fa2f
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-06-22 11:05:38 +0600

    Introduction to JavaScript

---
+++ Day 37 (JavaScript Introduction)/new.js
@@ -0,0 +1,3 @@
+console.log("JS is Awesome 1")
+console.log("JS is Awesome 2")
+console.log("JS is Awesome 3")
---
+++ Day 37 (JavaScript Introduction)/script.js
@@ -0,0 +1,20 @@
+alert("Hello World I am learning JS from today In Shaa Allah")
+
+console.log("JS is Awesome 1")
+console.log("JS is Awesome 2")
+console.log("JS is Awesome 3")
+
+var x = prompt("Enter Your Phone Number")
+console.log("Your Phone Number is " + x)
+
+var isTrue = confirm("Are You Want to Close This Window?")
+
+if (isTrue){
+    alert("Window is terminating!")
+    console.log("Window is terminating!")
+}
+
+else{
+    alert("Staying in the window")
+    console.log("Staying in the window")
+}
