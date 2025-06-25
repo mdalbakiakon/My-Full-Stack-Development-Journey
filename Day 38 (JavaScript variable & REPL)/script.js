1ac991931c231804a96b620a85a3aed78993e5ba
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-06-25 11:51:04 +0600

    Learning JS and DSA

---
+++ Day 38 (JavaScript variable & REPL)/script.js
@@ -0,0 +1,31 @@
+console.log("Hey I am learning JS");
+
+var a = 5;
+var b = 6;
+var c = "Md. Al Baki Akon";
+
+console.log(a + b); //this will return 11
+console.log("Value: " + a + b); //this will return 56 
+
+// ? this is string concatanation so value: + 5 becoming value: 5 than + 6 will make it ultimately 56
+
+// | how to solve it then??
+
+console.log("Value: " + (a + b));
+// now it will show value: 11
+
+console.log(typeof a + typeof b + typeof c);
+console.log(typeof a, typeof b, typeof c);
+//* same as python in js , will make extra whitespace while + will not 
+
+
+console.log("value of a" , a);
+a = a + 1;
+console.log("value of a" , a);
+
+const a1 = 9;
+console.log(typeof a1);
+console.log(a1);
+
+// a1 = a1 + 2 
+//! this is can not be done as the a1 is a constant
