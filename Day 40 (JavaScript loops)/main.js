7e021f0dae11117237d080c25c7406135ea16077
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-08-16 08:44:57 +0600

    learning about loop in JS

--- Day 40 (JavaScript loops)/main.js
+++ Day 40 (JavaScript loops)/main.js
@@ -1,5 +1,14 @@
 console.log("I am Learning Loops");
 
+// There is 5 kinds of loop in JS
+// for loop 
+// for in loop
+// for of loop
+// while loop
+// do while loop
+
+
+
 // basic for loop
 
 for(let i=0; i<10; i++){
@@ -11,6 +20,7 @@ console.log("\n");
 for(let i=0; i<10; ++i){
     console.log(i);
 }
+
 
 let a = 5;
 console.log("a: ", a)
@@ -33,9 +43,56 @@ let obj = {
     Profession: "SWE"
 }
 
-for (let key in obj) {
-    const element = obj[key];
-    console.log(key, element);
+
+
+console.log('\na: ', a, '\n')
+
+
+// basic for loop
+for(let i=0; i<100; i++){
+    console.log(a + i)
 }
 
 
+
+// for in loop used for dictionary/object
+let my_obj = {
+    name:"Siam",
+    role:"Software Engineer"
+}
+
+for(const key in my_obj){
+    let value = my_obj[key]
+    console.log(key, value)
+}
+
+
+
+// for iteration/ Array----------->in python we use in but here it is in reverse so we use of
+for (const c of 'Siam') {
+    console.log(c)
+}
+
+let fruits = ['Apple', 'banana', 'cherry']
+
+for(const fruit of fruits){
+    console.log(fruit)
+}
+
+
+// while loop
+let k = 0
+while(k<6){
+    console.log(k);
+    k++;
+}
+
+
+console.log('\n')
+
+
+// dowhile loop
+let j = 0
+do{
+    console.log(j)
+}while(j<0);
