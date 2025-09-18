e616781ae61ae490146057ef8bf411c1bca6e320
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-09-18 10:17:32 +0600

    Exercising with JS reduce

---
+++ Day 43 (JS Array)/Exercise/main.js
@@ -0,0 +1,13 @@
+// Factorial using loop and reduce
+
+let arr = []
+let x = parseInt(prompt("Enter Number: "))
+
+while(x != 0){
+    arr.push(x)
+    x = x-1
+}
+
+
+console.log(arr.reduce((a,b) => a*b, 1))
+
