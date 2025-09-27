c38d1e3f9f3e43d1a361a91ebd3672e57b631110
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-09-27 06:28:21 +0600

    Learning event and event handler in JS

--- Day 48 (JS DOM5)/main.js
+++ Day 48 (JS DOM5)/main.js
@@ -14,3 +14,38 @@ btn.addEventListener("click", ()=>{
     }
 })
 
+
+btn.addEventListener("contextmenu", ()=>{
+    // alert("Right mouse clicked!!!!")
+    document.querySelector(".box").style.cssText = `font-size: 24px;
+    font-weight: 700;
+    color: red;`
+})
+
+//!contextmenu is used for if someone right clicks!!!!
+
+
+// | Event Handling
+let elem = document.getElementById("hey")
+elem.onclick = function(){
+    alert("Hey clicked second time")
+}
+
+//! Note: adding an handler with JS overwrite the existing handler in HTML
+
+
+elem.addEventListener("mousemove", ()=>{
+    elem.style.backgroundColor = "red"
+})
+
+elem.addEventListener("mouseout", ()=>{
+    elem.style.fontSize = "32px"
+    elem.style.display = "block"
+})
+
+
+// elem.removeEventListener("mousemove", ()=>{
+//     elem.style.backgroundColor = "white"
+// })
+
+
--- Tailwind.md
+++ Tailwind.md
@@ -65,6 +65,29 @@ module.exports = {
 };
 ```
 
+```js
+// this is better way
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], //custom font [add in html from google as well]
+      },
+      colors: {
+        primary: "#121212",   // custom blue
+        accent: "#F97316",  // custom orange
+      },
+    },
+  },
+  plugins: [],
+}
+```
+
 For React projects, add:
 
 ```js
