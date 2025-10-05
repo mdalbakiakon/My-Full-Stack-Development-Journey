5d7f4ca284e6ab74fe668de9e2a2c1b271ee0f43
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-10-05 13:52:21 +0600

    Learning basics of Promise and it's methods

--- Day 49 (JS callback promises)/my_promise.js
+++ Day 49 (JS callback promises)/my_promise.js
@@ -1,33 +1,33 @@
 console.log("Hey I am learning promise today");
 
-let prom1 = new Promise((resolve, reject) => {
-  let a = Math.random();
-  if (a < 0.5) {
-    reject("Hey I am rejected!");
-  } else {
-    setTimeout(() => {
-      resolve("Hey I am resolved");
-    }, 3000);
-  }
-});
+// let prom1 = new Promise((resolve, reject) => {
+//   let a = Math.random();
+//   if (a < 0.5) {
+//     reject("Hey I am rejected!");
+//   } else {
+//     setTimeout(() => {
+//       resolve("Hey I am resolved");
+//     }, 3000);
+//   }
+// });
 
-// prom1.then((a)=>{
-//     console.log(a)
-// }).catch((error)=>{
-//     console.log(error)
-//     console.error(error)
-// })
+// // prom1.then((a)=>{
+// //     console.log(a)
+// // }).catch((error)=>{
+// //     console.log(error)
+// //     console.error(error)
+// // })
 
-let prom2 = new Promise((resolve, reject) => {
-  let a = Math.random();
-  if (a < 0.5) {
-    reject("Hey I am rejected! 2");
-  } else {
-    setTimeout(() => {
-      resolve("Hey I am resolved 2");
-    }, 3000);
-  }
-});
+// let prom2 = new Promise((resolve, reject) => {
+//   let a = Math.random();
+//   if (a < 0.5) {
+//     reject("Hey I am rejected! 2");
+//   } else {
+//     setTimeout(() => {
+//       resolve("Hey I am resolved 2");
+//     }, 3000);
+//   }
+// });
 
 // let p3 = Promise.all([prom1, prom2]); //we have to pass a list remember that
 
@@ -67,18 +67,58 @@ let prom2 = new Promise((resolve, reject) => {
     // again if anyone rejects it will print the rejected one and also then part will work as it will print the value with everything
 
 
-console.log("race")
+// console.log("race")
 
-let p5 = Promise.race([prom1, prom2])
-p5.then(value=>console.log(value))
-.catch(err=>{
-    console.log(err)
-    console.error(err)
+// let p5 = Promise.race([prom1, prom2])
+// p5.then(value=>console.log(value))
+// .catch(err=>{
+//     console.log(err)
+//     console.error(err)
+// })
+
+
+// | Method               | Resolves when                     | Rejects when                      | Notes                             |
+// | -------------------- | --------------------------------- | --------------------------------- | --------------------------------- |
+// | `Promise.all`        | All fulfill                       | Any one rejects                       | Waits for all                     |
+// | `Promise.allSettled` | Always                            | Never rejects                     | Returns statuses                  |
+// | `Promise.race`       | First settles (resolve or reject) | First settles (resolve or reject) | Fastest wins                      |
+// | `Promise.any`        | First fulfills                    | All reject                        | Ignores rejections until all fail |
+
+
+// .any .resolve .reject
+
+let try_promise1 = new Promise((resolve, reject)=>{
+    let x = Math.random()
+    if (x<0.5){
+        resolve("Resolved!")
+    }
+    else{
+        reject("Rejected!")
+    }
 })
 
+// try_promise1.then(value=>console.log(value))
+// .catch(err=>console.log(err))
 
-// | Behavior                    | Description                                           |
-// | -----------------------     |----------------------------------------------------- |
-// | 🏎️ `Promise.race`          | Settles with the **first** result (resolve or reject) |
-// | ✅ `Promise.all`           | Waits for **all** to resolve (fails if any reject)    |
-// | ⚖️ `Promise.allSettled`    | Waits for all, regardless of success/failure          |
+
+try_promise1 = Promise.resolve("Hurray!!!")
+// it will remove everything in the previous Promise and make a fully new one promise in the try_promise1 variable with only resolve value
+try_promise1 = Promise.reject("Oh Man!!!")
+// overwritten again
+// so only Oh Man is exist
+
+try_promise1.then(value=>console.log(value))
+.catch(err=>console.log(err))
+
+
+let try_promise2 = new Promise((_,reject)=>
+    {
+        reject("finally I am resolved")
+    })
+
+let px = Promise.any([try_promise1, try_promise2])
+px.then(value=>console.log(value))
+.catch(err=>console.log(err))
+
+// .any is the same as race race will print the first one whatsoever it is resolve or reject
+// but any will wait until the first resolved occured if not it will say AggregateError
