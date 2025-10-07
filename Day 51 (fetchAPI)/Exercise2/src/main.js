e9152b93c90a41aba60e1b7dfa03a5624bbe431b
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-10-07 06:27:07 +0600

    Clearingout the basic of Async and Await with exercises

--- Day 51 (fetchAPI)/Exercise2/src/main.js
+++ Day 51 (fetchAPI)/Exercise2/src/main.js
@@ -1,53 +1,44 @@
 const container = document.getElementById("container")
 
-function randomDelayTimer() {
-    return ((Math.ceil(Math.random() * 7))*1000)
+function delayTimer() {
+    return Math.ceil(Math.random()*7) * 1000
 }
 
-let taskList = ["Initializing hacking",
-    "Reading files",
-    "Uploading data to server"
+let myTask = ['Initializing hacking', 
+    'Reading personal files',
+    'Uploading data to server'
 ]
 
 
-// async func always return a promise no matter what!
-
-
-async function three_dot(el){
-    return new Promise((resolve) => {
-        
+async function three_dot(el) {
+    return new Promise((resolve) => {    
         let span = document.createElement("span")
         el.appendChild(span)
         
         let dot = ""
-        
-        let interval = setInterval(() => {
+        let interval = setInterval(()=>{
             dot = dot === "..." ? "" : dot + "."
             span.innerText = dot
-        }, 500)
+        },500)
         
         setTimeout(()=>{
             clearInterval(interval)
             span.innerText = "...done!"
             resolve()
-        }, randomDelayTimer())
+        }, delayTimer())
     })
 }
 
 
-
-async function main() {
-    // forEach dont work for await!!!
-    for (const t of taskList) { 
-        await new Promise((resolve) => {
-            setTimeout(resolve, 800)
-        })
-        const h1 = document.createElement("h1")
+async function main(){
+    for (const t of myTask) {
+        await new Promise(resolve => setTimeout(resolve, 800))
+        
+        let h1 = document.createElement("h1")
         h1.innerText = t
         container.appendChild(h1)
         await three_dot(h1)
     }
-    
 }
 
 main()
