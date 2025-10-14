818b2177741d9b5f053c567be9700e985f506872
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-10-14 15:53:37 +0600

    building E-commerce system

---
+++ Day 56 (Exercise)/prob13/src/asset/products.js
@@ -0,0 +1,34 @@
+export let products = [
+    {
+        p_id:"001",
+        p_img: "https://img-prd-pim.poorvika.com/cdn-cgi/image/width=1600,height=1600,quality=75/product/Apple-iphone-17-sage-512gb-Front-Back-Side-View.png",
+        p_name: "Iphone 17",
+        p_price: "899"
+    },
+
+    {
+        p_id:"002",
+        p_img: "https://inventstore.in/wp-content/uploads/2024/09/39-600x600.webp",
+        p_name: "Beats Studio Buds",
+        p_price: "125"
+    },
+
+    {
+        p_id:"003",
+        p_img: "https://inventstore.in/wp-content/uploads/2024/09/53-600x600.webp",
+        p_name: "Apple Watch",
+        p_price: "85"
+    },
+    {
+        p_id:"004",
+        p_img: "https://inventstore.in/wp-content/uploads/2024/11/2-600x461.webp",
+        p_name: "Macbook Air 14'",
+        p_price: "1750"
+    },
+    {
+        p_id:"005",
+        p_img: "https://img-prd-pim.poorvika.com/cdn-cgi/image/width=1600,height=1600,quality=75/product/Apple-iphone-air-space-black-512gb-Front-Back-Side-View.png",
+        p_name: "Iphone 17 Air",
+        p_price: "989"
+    }
+]
