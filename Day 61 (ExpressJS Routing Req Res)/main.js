03ee03b6a3fbfbef40882924b5f0cb374737d5d2
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-11-18 23:05:04 +0600

    Understanding properly the use of get and post methods

--- Day 61 (ExpressJS Routing Req Res)/main.js
+++ Day 61 (ExpressJS Routing Req Res)/main.js
@@ -2,9 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// get request has limitation say 2083 character in the URL otherwise giving 414 error which is too long URL
+// 2KB~8KB is the size of get request
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log('This is a get request') //console just print in the running terminal
+  res.send('Hello World!') //res send the info to the direct html page
 })
+
+
+app.use(express.static('public'))
+
+
+app.post('/', (req, res)=>{
+  console.log('This is a post request')
+  res.send('post request')
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
