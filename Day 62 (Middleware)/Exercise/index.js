f38089c5f273b3185193ce1a1a1c7795930c7f5a
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-11-26 09:01:08 +0600

    Exercising on organising folder using fs

--- Day 62 (Middleware)/Exercise/index.js
+++ Day 62 (Middleware)/Exercise/index.js
@@ -1,21 +1,30 @@
-const path = require("path")
-const fs = require("fs")
+const fs = require('fs')
+const path = require('path')
+const folder_path = './myFolder'
 
-const folderPath = './myFolder'
+
 let ext_set = new Set()
 
-fs.readdirSync(folderPath).forEach(file => {
-    ext_set.add(path.extname(file).slice(1,))    
+fs.readdirSync(folder_path).forEach(file => {
+    const file_path = path.join(folder_path, file)
+    ext_set.add(path.extname(file_path).slice(1))
 })
 
 const ext_list = [...ext_set]
 
-ext_list.forEach(ext=>{
-    fs.mkdirSync(`${folderPath}/${ext}`, {recursive:true})
-})
+ext_list.forEach(ext => {
+    const newFolder = path.join(folder_path, ext)
 
-fs.readdirSync(folderPath).forEach(file => {
-    if (fs.lstatSync(`${folderPath}/${file}`).isFile()){
-        fs.renameSync(`${folderPath}/${file}`, `${folderPath}/${path.extname(file).slice(1,)}/${file}`)
+    if (!fs.existsSync(newFolder)) {
+        fs.mkdirSync(newFolder)
     }
 })
+
+
+fs.readdirSync(folder_path).forEach(file => {
+    const file_path = path.join(folder_path, file)
+    if (fs.lstatSync(file_path).isFile()){
+        fs.renameSync(file_path, `${folder_path}/${path.extname(file_path).slice(1)}/${file}`)
+    }
+})
+
