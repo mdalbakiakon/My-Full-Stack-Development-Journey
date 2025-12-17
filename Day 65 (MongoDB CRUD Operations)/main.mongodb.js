9a08cf15ce2f8075ac48c5218b0ab7c0e38459d7
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2025-12-17 08:21:42 +0600

    Clearing out very vital basics of MongoDB playground and queries

--- Day 65 (MongoDB CRUD Operations)/main.mongodb.js
+++ Day 65 (MongoDB CRUD Operations)/main.mongodb.js
@@ -23,3 +23,61 @@ db.CRUD_Collection.find().pretty()
 db.CRUD_Collection.find({
     Age: { $gt: 18 }
 }).pretty()
+
+
+//| When you use .pretty() in MongoDB Playground (or mongosh scripts), only the last .pretty() output is shown.
+
+let a = db.CRUD_Collection.find().pretty()
+let b = db.CRUD_Collection.find({
+    Age: { $gt: 18 }
+}).pretty()
+
+
+console.log(a)
+console.log("\n\n\n")
+console.log(b)
+
+
+let c = db.CRUD_Collection.find({
+    Name:"Mishkath"
+}).pretty()
+
+//! Remember the key's are case sensitive!!!
+
+console.log("\n\n\n")
+console.log(c)
+
+
+// * the MongoDB Playeground considers the last query only therefor the result is not showing anything 
+
+
+/*
+let c = db.CRUD_Collection.find({
+    Name: "Mishkath"
+}).pretty()
+*/
+
+// ? the find() or pretty() is query but here we have saved the query value to a variable c so our last query is not invoking but inserted into a variable therefor the result is showing undefined
+
+
+console.log(b.count())
+// b.count().pretty()
+
+// | pretty works only on cursor cursor is a MongoDB query returned object so we can apply pretty on them but here the count() is returning a value a counter of query b so it is not a cursor therefor pretty is not showing anything
+
+d = db.CRUD_Collection.find({
+    Name: "Siam"
+})
+
+console.log("\n\n\n")
+console.log(d.toArray())
+
+// to use toArray we must ensure that it is a fresh query not using pretty or anything extra 
+// to array return a list of the document of the query
+
+console.log(typeof(d.toArray()))
+//| in real sense it is an array but in JS typeof return object for an array
+
+console.log(Array.isArray(d.toArray()))
+// see it returns true
+// | so arrays are object type in JS
