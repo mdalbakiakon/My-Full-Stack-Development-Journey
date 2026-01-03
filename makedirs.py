03d3c0c27aaffc4a24e0d85f00e88c697240be41
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-01-03 23:34:45 +0600

    Learning difference why React is important

--- makedirs.py
+++ makedirs.py
@@ -1,6 +1,6 @@
 import os
 
 folder_path = 'D:\\Working on Programme\\Full-Stack-Development-Journey'
-new_folder_path = os.path.join(folder_path, 'Day 68 (Mongoose Basic Exercise 2)')
+new_folder_path = os.path.join(folder_path, 'Day 69 (React Intro)')
 
 os.makedirs(new_folder_path, exist_ok=True)
