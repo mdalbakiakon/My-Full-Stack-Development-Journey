f205dbb3642b5279d2faeab467a4e42a97c10cc9
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-01-04 23:18:56 +0600

    Learning React useState

--- Day 69 (React Intro)/my-app/src/App.js
+++ Day 69 (React Intro)/my-app/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState(0)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Counter'>{value}</div>
+      <button onClick={()=>{setValue(value+1)}}>Click Me</button>
     </div>
   );
 }
