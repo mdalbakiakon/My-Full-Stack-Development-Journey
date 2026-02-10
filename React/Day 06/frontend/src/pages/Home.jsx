f777de4d2bc229f842bd6373da7fcb5e05dfc2d3
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-02-10 12:32:05 +0600

    Entering into professional work

--- React/Day 06/frontend/src/pages/Home.jsx
+++ React/Day 06/frontend/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="w-full h-dvh flex flex-col gap-5 md:gap-10 justify-center items-center px-4 text-center relative">
           <h1 className="title text-white text-[42px] md:text-6xl select-none text-nowrap leading-10 md:leading-15 text-shadow-2xl">
             Your<span className="text-transparent bg-[linear-gradient(45deg,#F6CD02,#FE9A00)] bg-clip-text"> Adventures </span>
-            <br /> begins here
+            <br /> begin here
           </h1>
           <p className="subtitle text-lg lg:text-xl text-white text-shadow-xl opacity-35 select-none">
             Escape the mundane and find spark in the heart of nature’s wonders
--- makedirs.py
+++ makedirs.py
@@ -1,6 +1,6 @@
 import os
 
 folder_path = 'D:\\Working on Programme\\Full-Stack-Development-Journey'
-new_folder_path = os.path.join(folder_path, 'Day 71 (React Hooks_State)')
+new_folder_path = os.path.join(folder_path, 'Day 72 (React Hooks_useEffect)')
 
 os.makedirs(new_folder_path, exist_ok=True)
