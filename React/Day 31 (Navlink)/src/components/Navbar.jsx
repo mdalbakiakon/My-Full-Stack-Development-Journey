3e59570a42730fd1cea089c6c0bce4576fa4e994
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-08-16 09:21:15 +0600

    chore: learnt about using NavLink from react-router-dom

---
+++ React/Day 31 (Navlink)/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-12.5 rounded-xl flex justify-between items-center'>
+        <span className='font-semibold text-2xl'>Logo</span>
+
+        <nav className='flex justify-center items-center gap-5'>
+            <NavLink to='/' className={({isActive})=> isActive ? "text-red-500" : ""}>Home</NavLink>
+            <NavLink to='/about' className={({isActive})=> isActive ? "text-red-500" : ""}>About</NavLink>
+            <NavLink to='/contact' className={({isActive})=> isActive ? "text-red-500" : ""}>Contact</NavLink>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
