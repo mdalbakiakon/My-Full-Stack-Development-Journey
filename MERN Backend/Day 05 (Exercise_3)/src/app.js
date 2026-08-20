9ee84ccb8347f5b9abec529cd43a311d1b84bd30
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-08-20 23:12:12 +0600

    chore: learnt fully CRUD operation

--- MERN Backend/Day 05 (Exercise_3)/src/app.js
+++ MERN Backend/Day 05 (Exercise_3)/src/app.js
@@ -25,10 +25,50 @@ app.post('/courses', async (req, res) => {
 // GET /courses -- get all courses
 app.get('/courses', async (req, res) => {
     const courses = await coursesModel.find();
+
+    const oneCourse = await coursesModel.findOne({
+        course_name: 'React frontend'
+    });
+
     res.status(200).json({
         message: 'all courses fetched successfully',
-        courses: courses
+        courses: courses,
+        oneCourse: oneCourse
     })
+
+    // find() => [{}, {}, ...] or []
+    // findOne() => {} or null
+
 });
+
+
+// DELETE /courses/:id -- delete course
+app.delete('/courses/:id', async (req, res) => {
+    const id = req.params.id;
+    await coursesModel.findOneAndDelete({
+        _id: id
+    });
+
+    res.status(200).json({
+        message: 'course deleted successfully'
+    });
+});
+
+
+// PATCH /notes/:id -- update course
+app.patch('/courses/:id', async (req, res) => {
+    const id = req.params.id;
+    const course_name = req.body.course_name;
+
+    await coursesModel.findOneAndUpdate({
+        _id: id
+    }, {
+        course_name: course_name
+    });
+
+    res.status(200).json({
+        message: 'course updated successfully'
+    });
+})
 
 export default app;
