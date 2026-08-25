6efebc76daaf57999429a7decc75e41218c5c14f
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-08-25 12:20:50 +0600

    fix: removed the redundent try catch block in db.js file as server.js is handling the outer catch block

--- MERN Backend/Project 02 (Spotify)/src/db/db.js
+++ MERN Backend/Project 02 (Spotify)/src/db/db.js
@@ -1,13 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_DB);
-        console.log('successfully connected to database')
-    } catch (error) {
-        console.log(error)
-        throw error;
-    }
+    await mongoose.connect(process.env.MONGODB_DB);
+    console.log('successfully connected to database');
 }
 
 export default connectDB;
