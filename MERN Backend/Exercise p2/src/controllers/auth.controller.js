b2a34df4564337ee8a36c4ac9ce1524bb1e9ce34
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-08-26 15:00:25 +0600

    chore: completed login api authentication

--- MERN Backend/Exercise p2/src/controllers/auth.controller.js
+++ MERN Backend/Exercise p2/src/controllers/auth.controller.js
@@ -39,7 +39,7 @@ const signupUser = async (req, res) => {
 
         // error handle for any invalid credentials used in signup
         if (error.name === "ValidationError") {
-            
+
             // making a object of errors that contains all validation errors together to show in the response
             const foundError = {};
             Object.values(error.errors).forEach((err) => {
@@ -67,4 +67,74 @@ const signupUser = async (req, res) => {
     }
 }
 
-export default {signupUser};
+
+// POST /api/auth/login -- login user
+const loginUser = async (req, res) => {
+    try {
+        // getting user input
+        let { identifier, password } = req.body;
+
+        // as db schema set to trim the identifiers so we are triming to increase if user gives a valid value with extra space      
+        identifier = identifier.trim();
+
+        // if user dont give any credentials
+        if (!identifier || !password) {
+            return res.status(400).json({
+                message: 'identifier and password are required'
+            });
+        }
+
+        // finding user from database
+        const foundUser = await userModel.findOne({
+            $or: [{ email: identifier }, { username: identifier }]
+        })
+
+        // if user not found from database
+        if (!foundUser) {
+            return res.status(401).json({
+                message: 'unauthorized user'
+            })
+        }
+
+        // comparing password from hashed password
+        const isPasswordValid = await foundUser.comparePassword(password);
+
+        // if user did not give valid password
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                message: 'unauthorized user'
+            })
+        }
+
+        // setting up login token
+        const token = jwt.sign({
+            id: foundUser._id,
+            role: foundUser.role
+        }, process.env.JWT_SECRET);
+
+        // saving token in cookies
+        res.cookie("EXERCISE_TOKEN", token);
+
+        // showUser
+        const showUser = foundUser.toObject();
+        delete showUser.password;
+
+        // successful login
+        return res.status(200).json({
+            message: 'login successful',
+            user: showUser
+        })
+
+    } catch (error) {
+        console.log(error.message);
+
+        // fallback error handling
+        return res.status(500).json({
+            message: 'something went wrong',
+            error: error.message
+        })
+    }
+}
+
+
+export default { signupUser, loginUser };
--- MERN Backend/Exercise p2/src/routes/auth.routes.js
+++ MERN Backend/Exercise p2/src/routes/auth.routes.js
@@ -4,6 +4,9 @@ import authController from "../controllers/auth.controller.js";
 const router = express.Router();
 
 // post /signup -- register new user
-router.post('/signup', authController.signupUser)
+router.post('/signup', authController.signupUser);
+
+// post /login -- login user
+router.post('/login', authController.loginUser);
 
 export default router;
